cae9e757f181a868640ac9a7cedac5b57ddc6eef
Santiago Mattiauda <santimattius@gmail.com>
2021-05-09 21:01:20 -0300

    [testing] add unit testing

--- app/src/test/java/com/santimattius/template/domain/usecases/GetPicturesTest.kt
+++ app/src/test/java/com/santimattius/template/domain/usecases/GetPicturesTest.kt
@@ -1,5 +1,37 @@
 package com.santimattius.template.domain.usecases
 
-import org.junit.Assert.*
+import com.santimattius.template.domain.repositories.PicturesRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
 
-class GetPicturesTest
+@ExperimentalCoroutinesApi
+class GetPicturesTest {
+
+    private lateinit var repository: PicturesRepository
+    private lateinit var useCase: GetPictures
+
+    @Before
+    fun setUp() {
+        repository = mockk()
+        useCase = GetPictures(repository)
+
+    }
+
+    @Test
+    fun `invoke get picture use case`() = runBlockingTest {
+
+        coEvery { repository.getPictures() } returns emptyList()
+
+        val result = useCase.invoke()
+
+        assert(result.isEmpty())
+
+        coVerify { repository.getPictures() }
+
+    }
+}
